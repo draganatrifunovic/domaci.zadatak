2e99456293ea708357dee585ab83dd4d05df7fbb
Dragana Trifunovic <gaga_trifunovic@hotmail.com>
2020-02-12 00:14:14 +0100

    added Drugi Dan

---
+++ nedelja3/zadatak1.js
@@ -0,0 +1,6 @@
+function maximum(num1, num2, num3) {
+    return Math.max(num1,num2,num3);
+}
+
+let maximumOfnum = maximum(3,55,7);
+console.log(maximumOfnum);
---
+++ nedelja3/zadatak2.js
@@ -0,0 +1,6 @@
+function racunanjeCene(poluprecnik, cena) {
+    return   poluprecnik *cena;
+}
+
+let cenaPokvadratnomCentimetru = racunanjeCene(6,160);
+console.log(cenaPokvadratnomCentimetru);
